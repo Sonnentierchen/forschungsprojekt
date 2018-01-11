cb497cde46a5ce31a7e0372c52334b7e852c4fda
Florian Blume <flob.91@gmx.de>
2018-01-11 13:25:25 +0100

    Made names of files clearer.

--- compare_coco_vufo_precision.py
+++ compare_coco_vufo_precision.py
@@ -4,10 +4,10 @@ cocoImagesPath = "./images/images_coco_excerpt/"
 cocoImagesOutputPath = "./images_output/images_coco/"
 cocoBatchSize = 5
 
-csb.calc_save_bbs(cocoImagesPath, cocoBatchSize, cocoImagesOutputPath, True)
+cocoBBs = csb.calc_save_bbs(cocoImagesPath, cocoBatchSize, cocoImagesOutputPath, True)
 
 vufoImagesPath = "./images/images_vufo/"
 vufoImagesOutputPath = "./images_output/images_vufo/"
 vufoBatchSize = 3
 
-csb.calc_save_bbs(vufoImagesPath, vufoBatchSize, vufoImagesOutputPath, True)
+vufoBBs = csb.calc_save_bbs(vufoImagesPath, vufoBatchSize, vufoImagesOutputPath, True)

52272027baca970e7c533e3b0d3ac475f2ce8970
Florian Blume <flob.91@gmx.de>
2018-02-22 22:52:53 +0100

    Fixed implementation of data extractor.

--- util/extract_data_by_key_from_via_annotations.py
+++ util/extract_data_by_key_from_via_annotations.py
@@ -6,14 +6,13 @@ def extract_data_by_key_from_via_annotations(annotations_path, key, output_path)
     if not os.path.exists(output_path):
         os.makedirs(output_path)
 
-    annotations_file_name, extension = os.path.splitext(annotations_path)
+    annotations_file_name = os.path.basename(annotations_path)
+    annotations_file_name, extension = os.path.splitext(annotations_file_name)
     modified_annotations_file_name = annotations_file_name + "_" + key + extension
 
-    with open(annotations_path, "r") as json_file, open(os.paht.join(output_path, modified_annotations_file_name), "w") as json_output_file:
+    with open(annotations_path, "r") as json_file, open(os.path.join(output_path, modified_annotations_file_name), "w") as json_output_file:
         json_data = json.load(json_file)
         output_json_data = {entry : json_data[entry] for entry in json_data if key in entry}
-        print("Copied the following entries:")
-        for key in json_data: print(key)
         json.dump(output_json_data, json_output_file)
 
 if __name__ == '__main__':
